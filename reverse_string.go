e5f18e436847c01508a03519692dddbe44cf8aed
Alina <you@example.com>
2022-05-18 14:39:01 +0300

    hw_4.1

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,13 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	runes := []rune(input)
+	newRunes := make([]rune, len(runes))
+	count := 0
+	for i := len(runes) - 1; i >= 0; i-- {
+		newRunes[count] = runes[i]
+		count++
+	}
+	output = string(newRunes)
 	return output
 }
